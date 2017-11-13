3b76e0e8de8ea6203013cd23eb67344b38598d56
Booharin <31410351+Booharin@users.noreply.github.com>
2017-11-13 18:49:53 +0300

    Added Chat

---
+++ vk/Chat.swift
@@ -0,0 +1,9 @@
+//
+//  Chat.swift
+//  vk
+//
+//  Created by Александр on 12.11.2017.
+//  Copyright © 2017 Александр. All rights reserved.
+//
+
+import Foundation
---
+++ vk/ChatMethods.swift
@@ -0,0 +1,9 @@
+//
+//  ChatMethods.swift
+//  vk
+//
+//  Created by Александр on 13.11.2017.
+//  Copyright © 2017 Александр. All rights reserved.
+//
+
+import Foundation
