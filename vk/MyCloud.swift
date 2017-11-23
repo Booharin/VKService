5d30e8992195618df4945c41b10055bbf9313ac5
Booharin <31410351+Booharin@users.noreply.github.com>
2017-11-23 17:11:50 +0300

    Realm update

---
+++ vk/MyCloud.swift
@@ -0,0 +1,9 @@
+//
+//  MyCloud.swift
+//  vk
+//
+//  Created by Александр on 22.11.2017.
+//  Copyright © 2017 Александр. All rights reserved.
+//
+
+import Foundation
---
+++ vk/RealmMethodsForGroups.swift
@@ -0,0 +1,9 @@
+//
+//  RealmMethodsForGroups.swift
+//  vk
+//
+//  Created by Александр on 23.11.2017.
+//  Copyright © 2017 Александр. All rights reserved.
+//
+
+import Foundation
