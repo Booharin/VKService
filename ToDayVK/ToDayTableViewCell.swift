e1630f6b944fcd25eaef64f8d060b3d2063ca127
Booharin <31410351+Booharin@users.noreply.github.com>
2017-11-28 17:40:19 +0300

    Extension added

---
+++ ToDayVK/ToDayTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDayTableViewCell.swift
+//  ToDayVK
+//
+//  Created by Александр on 27.11.2017.
+//  Copyright © 2017 Александр. All rights reserved.
+//
+
+import UIKit
+
+class ToDayTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
