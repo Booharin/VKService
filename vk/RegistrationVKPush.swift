282e20a41ca0888115e32021669e5fac01536627
Booharin <31410351+Booharin@users.noreply.github.com>
2017-12-13 17:26:38 +0300

    Chat updates, VKPushes added

---
+++ vk/RegistrationVKPush.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  vk
+//
+//  Created by Александр on 10.12.2017.
+//  Copyright © 2017 Александр. All rights reserved.
+//
+
+import Foundation
---
+++ vk/RemoteNotification.swift
@@ -0,0 +1,9 @@
+//
+//  RemoteNotification.swift
+//  vk
+//
+//  Created by Александр on 08.12.2017.
+//  Copyright © 2017 Александр. All rights reserved.
+//
+
+import Foundation
